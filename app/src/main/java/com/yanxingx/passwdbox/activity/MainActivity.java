0dee38b525b017d0f5f034eb975d2e3e02d665a1
yanxingx <1076359289@qq.com>
2016-08-25 08:54:16 +0800

    2

---
+++ app/src/main/java/com/yanxingx/passwdbox/activity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.yanxingx.passwdbox.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.yanxingx.passwdbox.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+    private void initViews(){
+        findViewById(R.id.addCouAndPasswd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "hehe", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
